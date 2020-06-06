c22f70421e96bc8fdb861e458dabfaea7e42fec0
mrugankray <mrugankray2@gmail.com>
2020-06-06 16:38:46 +0530

    defined custom validating functions for id_card and reg_type and have added image size checking validation too.

--- register/serializers.py
+++ register/serializers.py
@@ -3,23 +3,37 @@ from .models import Registration
 import os
 
 
+def check_image_format(image):
+    file_name, ext = os.path.splitext(str(image))
+    accepted_ext = {'.png': True, '.jpeg': True}
+
+    if accepted_ext.get(ext, False):
+        return True
+    else:
+        raise serializers.ValidationError("Image format should be in png or jpeg format.")
+
+
+def check_image_size(image):
+    if image.size < 4 * 1024 * 1024:
+        return image
+    else:
+        raise serializers.ValidationError("Image size should be < 4 mb")
+
+
+def check_reg_type(reg_type):
+    accepted_reg_type = {'self': True, 'group': True, 'corporate': True, 'others': True}
+    reg_type_lower_case = reg_type.lower()
+    if accepted_reg_type.get(reg_type_lower_case, False):
+        reg_type = reg_type_lower_case
+        return reg_type
+    else:
+        raise serializers.ValidationError("Registration Type should be of 4 types i.e. Self, Group"
+                                          ", Corporate, Others.")
+
+
 class RegistrationSerializer(serializers.ModelSerializer):
-    def validate(self, data):
-        file_name, ext = os.path.splitext(str(data['id_card']))
-
-        accepted_ext = {'.png': True, '.jpeg': True}
-        accepted_reg_type = {'self': True, 'group': True, 'corporate': True, 'others': True}
-
-        reg_type_lower_case = data['reg_type'].lower()
-        if accepted_reg_type.get(reg_type_lower_case, False):
-            data['reg_type'] = reg_type_lower_case
-            if accepted_ext.get(ext, False):
-                return data
-            else:
-                raise serializers.ValidationError("Image format should be in png or jpeg format.")
-        else:
-            raise serializers.ValidationError("Registration Type should be of 4 types i.e. Self, Group"
-                                              ", Corporate, Others.")
+    id_card = serializers.ImageField(validators=[check_image_format, check_image_size])
+    reg_type = serializers.CharField(validators=[check_reg_type])
 
     class Meta:
         model = Registration
