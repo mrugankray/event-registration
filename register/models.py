b7a9f0286b3e82a37f03236b4fecc06da4171f5b
mrugankray <mrugankray2@gmail.com>
2020-06-06 14:08:16 +0530

    Added Custom validators to id_card and reg_type

--- register/models.py
+++ register/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 import os
-from rest_framework.response import Response
-from rest_framework import status
+'''from rest_framework.response import Response
+from rest_framework import status'''
+from django.core.exceptions import ValidationError
 
 
 class Registration(models.Model):
@@ -17,22 +18,3 @@ class Registration(models.Model):
 
     def __str__(self):
         return str(self.id)
-
-    def save(self, *args, **kwargs):
-        file_name, ext = os.path.splitext(self.id_card.path)
-
-        accepted_ext = {'.png': True, '.jpeg': True}
-        accepted_reg_type = {'self': True, 'group': True, 'corporate': True, 'others': True}
-        if accepted_ext.get(ext, False):
-            reg_type_lower_case = self.reg_type.lower()
-            if accepted_reg_type.get(reg_type_lower_case, False):
-                self.reg_type = reg_type_lower_case
-                super().save()
-            else:
-                return Response({
-                    "msg": "Registration Type should be of 4 types i.e. Self, Group, Corporate, Others."
-                }, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({
-                "msg": "Image format should be in png or jpeg format."
-            }, status=status.HTTP_400_BAD_REQUEST)
--- register/serializers.py
+++ register/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from .models import Registration
+import os
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        file_name, ext = os.path.splitext(str(data['id_card']))
+        print(ext)
+
+        accepted_ext = {'.png': True, '.jpeg': True}
+        accepted_reg_type = {'self': True, 'group': True, 'corporate': True, 'others': True}
+
+        reg_type_lower_case = data['reg_type'].lower()
+        if accepted_reg_type.get(reg_type_lower_case, False):
+            data['reg_type'] = reg_type_lower_case
+            if accepted_ext.get(ext, False):
+                return data
+            else:
+                raise serializers.ValidationError("Image format should be in png or jpeg format.")
+        else:
+            raise serializers.ValidationError("Registration Type should be of 4 types i.e. Self, Group"
+                                              ", Corporate, Others.")
+
     class Meta:
         model = Registration
         fields = '__all__'
