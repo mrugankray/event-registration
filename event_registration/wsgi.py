f36728bd42a362d6b48272281104db385f912f36
mrugankray <mrugankray2@gmail.com>
2020-06-07 18:19:38 +0530

    modified wwsgi.py file so that it can be deployed in pythonanywhere.

--- event_registration/wsgi.py
+++ event_registration/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_registration.settings')
+path = '/home/mrugankray/event-registration'
+if path not in sys.path:
+    sys.path.append(path)
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_registration.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'event_registration.settings'
 
 application = get_wsgi_application()
